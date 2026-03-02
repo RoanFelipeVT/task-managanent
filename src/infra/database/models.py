fd3d29c61b9082c1b5f811e70f7397bc719684d6
Roan Felipe <roanfelipest@gmail.com>
2026-03-02 19:39:29 -0300

    feat: adicionando novas tabelas ORM

--- src/infra/database/models.py
+++ src/infra/database/models.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from src.infra.database.config import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import LargeBinary
 
+
+task_tag = Table(
+    'task_tag',
+    Base.metadata,
+    Column('task_id', Integer, ForeignKey('task.id')),
+    Column('tag_id', Integer, ForeignKey('tag.id'))
+)
 
 
 class Tag(Base):
@@ -8,7 +17,10 @@ class Tag(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id', name='fk_user'))
 
+    user = relationship('User', back_populates='tags')
+    tasks = relationship('Task', secondary=task_tag, back_populates='tags')
 
 
 class Task(Base):
@@ -17,17 +29,21 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     date = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
+    user = relationship('User', back_populates='tasks')
+    tags = relationship('Tag', secondary=task_tag, back_populates='tasks')
 
 
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True, index = True)
-    name = Column(String) 
-    email = Column(String) 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
     password = Column(String)
+    cellphone = Column(String)
+    photo = Column(LargeBinary)
 
-
-class Habits(Base):
-    __tablename__ = 'table'
+    tasks = relationship('Task', back_populates='user')
+    tags = relationship('Tag', back_populates='user')
