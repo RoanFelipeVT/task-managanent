25262cdd8ae4c750ae990a02d520cf7d241333f6
Roan Felipe <roanfelipest@gmail.com>
2026-03-03 18:52:25 -0300

    chore(db): adicionando endpoints temporários de criar remover database tables para teste

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from src.infra.database.config import criar_db, drop_db
-import src.infra.database.models 
+from fastapi import FastAPI, HTTPException
+from src.infra.database.config import criar_db, drop_db, engine
+import src.infra.database.models
+from sqlalchemy import inspect
 
 
 
-criar_db()
 
 app = FastAPI()
 
@@ -12,4 +12,37 @@ app = FastAPI()
 @app.get("/teste")
 def read_root():
     return {"message": "Hello, World"}
+
+
+## Endpoint Utilizado Para Teste de Banco de dados
+@app.delete("/database/remove")
+def remove_db():
+
+    inspector = inspect(engine)
+
+    if not inspector.get_table_names():
+        raise HTTPException(
+            status_code=409,
+            detail="No tables to remove"
+        )
+
+    drop_db()
+
+    return {"message": "Database cleared"}
+
+## Endpoint Utilizado Para Teste de Banco de dados
+@app.post("/database/create")
+def create_db():
+
+    inspector = inspect(engine)
+
+    if inspector.get_table_names():
+        raise HTTPException(
+            status_code=409,
+            detail="Tables already exist"
+        )
+
+    criar_db()
+
+    return {"message": "Database initialized"}
 
