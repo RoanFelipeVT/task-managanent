3d3838e55f3bc6fa2f72ee1220a35799e0f7f4d1
Roan Felipe <roanfelipest@gmail.com>
2026-03-21 20:31:08 -0300

    feat: adicionar estrutura de configurações de envs

---
+++ config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Variáveis do Banco
+    DB_DIALECT: str = "postgresql"
+    DB_DRIVER: str = "psycopg2"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int = 5432
+    DB_NAME: str
+    
+
+
+    @property
+    def DATABASE_URL(self) -> str:
+        """Montagem da URL"""
+        return f"{self.DB_DIALECT}+{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
