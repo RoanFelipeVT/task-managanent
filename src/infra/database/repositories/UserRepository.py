7881e97dbb0cea4602ba3a63268ddfcbddc33a50
Roan Felipe <roanfelipest@gmail.com>
2026-07-20 11:56:16 -0300

    wip: mudando o trabalho para casa

--- src/infra/database/repositories/UserRepository.py
+++ src/infra/database/repositories/UserRepository.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from src.infra.database.models.user import User
 
 
+campos_proibidos = {"id"}
+
 class UserRepository:
 
     def __init__(self, db: Session):
@@ -12,3 +14,18 @@ class UserRepository:
         self.db.commit()
         self.db.refresh(user)
         return user
+
+    def update_field(self, user_id, update_data: dict) -> User:
+        user = self.db.query(User).filter(User.id == user_id).first()
+        if not user:
+            raise ValueError("Usuário não encontrado")
+
+        for key, value in update_data.itens():
+            if key not in campos_proibidos and hasattr(user, key):
+                setattr(user, key, value)
+
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    
